942f5a6fab61552ba427d9179bdb21f0c6b199de
Carlos Eduardo <dudumonti@hotmail.com>
2021-12-16 15:18:57 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department("Books", 1);
+		
+		System.out.print(obj);
+
+	}
+
+}
